43fb91586d7acd4981fcf8be8c6941b5eff9aba4
Matthew Chick <chikmatthew@gmail.com>
2017-11-18 08:40:25 +1100

    add eslint sudo npm install --save-dev eslint-config-rallycoding

---
+++ index.js
@@ -0,0 +1,19 @@
+/* import { AppRegistry } from 'react-native';
+import App from './App';
+
+AppRegistry.registerComponent('album', () => App); */
+
+// Import a library to help create a Component
+import React from 'react';
+import { Text, AppRegistry } from 'react-native'; //deconstruct
+
+// Create a Component
+/* const App = () => {
+  return <Text>Some Text</Text>;
+};
+*/
+const App = () => <Text>Some Text</Text>;
+
+// Render it to the device
+
+AppRegistry.registerComponent('album', () => App);

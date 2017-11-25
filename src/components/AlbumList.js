28397f268d4c9b68c14a428f04cdcb23536c9205
Matthew Chick <chikmatthew@gmail.com>
2017-11-26 08:22:52 +1100

    Create albumList component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -9,22 +9,31 @@ const AlbumList = () => {
       <Text>Album List !!!</Text>
     </View>
   );
-}; */
+};
+*/
 // class Component
 class AlbumList extends Component {
   state = { albums: [] };     //step 1: array of emptyObject
+
   //lifecycle -> https://reactjs.org/docs/react-component.html
   componentWillMount() {
     console.log('componentWillMount in AlbumList');
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
     .then(response => this.setState({ albums: response.data }));
   }
+  //map() is similar to forEach function
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <Text key={album.title}>{album.title}</Text>
+    );
+  }
+
   render() {
     console.log('State: ', this.state);
 
     return (
       <View>
-        <Text>Album List !!!</Text>
+        {this.renderAlbums()}
       </View>
     );
   }

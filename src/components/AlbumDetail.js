790448aefb42205a36c43067ace4fd2563d3c82d
Matthew Chick <chikmatthew@gmail.com>
2017-12-08 17:08:46 +1100

    Add headerContentStyle in AlbumDetail

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection'
-
 // pass props.albumdata.title into Card
 const AlbumDetail = (props) => {
  return (
    <Card>
      <CardSection>
-      <Text>{props.albumdata.title}</Text>
-     </CardSection>
-     
-     <CardSection>
-      <Text>{props.albumdata.title}</Text>
+     <View></View>
+     <View style={styles.headerContentStyle}>
+        <Text>{props.albumdata.title}</Text>
+        <Text>{props.albumdata.artist}</Text>
+     </View>
      </CardSection>
    </Card>
  );
 };
-
+const styles = {
+  headerContentStyle: {
+    justifyContent: 'flex-start',
+    flexDirection: 'column',
+  }
+};
 export default AlbumDetail;

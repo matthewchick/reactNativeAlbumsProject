b2ec704628b193fdf67f85d757c8d95a73f08af3
Matthew Chick <chikmatthew@gmail.com>
2017-12-16 08:49:46 +1100

    Create an artwork

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -4,22 +4,34 @@ import Card from './Card';
 import CardSection from './CardSection'
 // pass props.albumdata.title into Card
 const AlbumDetail = ({albumdata}) => {
-   const { title, artist, thumbnail_image } = albumdata;
-   const { thumbnailStyle, headerContentStyle } = styles;
+   const { title, artist, thumbnail_image, image } = albumdata;
+   const {
+     thumbnailStyle,
+     headerContentStyle,
+     thumbnailContainerStyle,
+     headerTextStyle,
+     imageStyle
+   } = styles;
 
    return (
      <Card>
        <CardSection>
-       <View>
+       <View style={thumbnailContainerStyle}>
           <Image
             style={thumbnailStyle}
             source={{ uri: thumbnail_image }}
           />
        </View>
        <View style={headerContentStyle}>
-          <Text>{title}</Text>
+          <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
        </View>
+       </CardSection>
+       <CardSection>
+           <Image
+             style={imageStyle}
+             source={{ uri: image }}
+           />
        </CardSection>
      </Card>
    );
@@ -29,9 +41,23 @@ const styles = {
     justifyContent: 'flex-start',
     flexDirection: 'column',
   },
+  headerTextStyle: {
+    fontSize: 18
+  },
   thumbnailStyle: {
     height: 50,
     width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 };
 export default AlbumDetail;
